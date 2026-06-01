44e4f4930e004545d7d7ac88bad85d91ea027056
Modjosan <133044820+Modjosan@users.noreply.github.com>
2026-06-01 09:03:59 +0200

    index

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'fitpro-v1';
+const FILES = ['index.html','manifest.json','icon-192.svg','icon-512.svg'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
